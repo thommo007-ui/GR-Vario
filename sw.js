324280c67fb073a467e0ea58bd5d047d4a31274f
Irluke <thommo007@gmail.com>
2026-07-07 14:05:25 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategy: on first successful load, cache this page. From then on,
 // try the network first (to pick up updates), but if that fails
 // (no signal in flight) fall back to the cached copy instantly.
-var CACHE = "glide-vario-cache-v37";
+var CACHE = "glide-vario-cache-v38";
 var ASSETS = [
   "./",
   "./index.html"
